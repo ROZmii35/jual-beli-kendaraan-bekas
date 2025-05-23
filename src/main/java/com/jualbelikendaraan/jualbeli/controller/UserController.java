85ef595167faf8d50c8423f02ebb156fe8ad158d
Angelina0905 <yudriangell@gmail>
2025-05-23 10:31:12 +0700

    first commit

---
+++ src/main/java/com/jualbelikendaraan/jualbeli/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.jualbelikendaraan.jualbeli.controller;
+
+import com.jualbelikendaraan.jualbeli.model.User;
+import com.jualbelikendaraan.jualbeli.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository udserRepository;
+         
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+    
+}
