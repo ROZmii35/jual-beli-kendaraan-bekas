6bc692e7982f1b013f8454e3a08a2df474fae7b2
unyubgtina <innadosinaen@gmail.com>
2025-05-23 10:05:29 +0700

    first commit

---
+++ src/main/java/com/jualbelikendaraan/jualbeli/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.jualbelikendaraan.jualbeli.repository;
+
+import com.jualbelikendaraan.jualbeli.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
