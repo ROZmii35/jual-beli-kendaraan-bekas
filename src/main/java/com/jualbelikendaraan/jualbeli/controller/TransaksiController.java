3aaccb3f0c398ff0cf6f22fe33b40631480c76fe
Angelina0905 <yudriangell@gmail>
2025-05-23 11:22:05 +0700

    second commit fix

--- src/main/java/com/jualbelikendaraan/jualbeli/controller/TransaksiController.java
+++ src/main/java/com/jualbelikendaraan/jualbeli/controller/TransaksiController.java
@@ -1,5 +1,27 @@
 package com.jualbelikendaraan.jualbeli.controller;
 
+import com.jualbelikendaraan.jualbeli.model.Transaksi;
+import com.jualbelikendaraan.jualbeli.repository.TransaksiRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/kendaraan")
 public class TransaksiController {
-    
+   @Autowired
+    private TransaksiRepository transaksiRepository;
+
+    @GetMapping
+    public List<Transaksi> getAllTransaksi() {
+        return transaksiRepository.findAll();
+    }
+
+    @PostMapping
+    public Transaksi createTransaksi(@RequestBody Transaksi transaksi) {
+        return transaksiRepository.save(transaksi);
+    }
+
 }
--- src/main/java/com/jualbelikendaraan/jualbeli/model/Kendaraan.java
+++ src/main/java/com/jualbelikendaraan/jualbeli/model/Kendaraan.java
@@ -15,33 +15,43 @@ public abstract class Kendaraan {
     private double harga;
 
     public abstract String getJenis();
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getMerk() {
         return merk;
     }
+
     public void setMerk(String merk) {
         this.merk = merk;
     }
+
     public String getTipe() {
         return tipe;
     }
+
     public void setTipe(String tipe) {
         this.tipe = tipe;
     }
+
     public int getTahun() {
         return tahun;
     }
+
     public void setTahun(int tahun) {
         this.tahun = tahun;
     }
+
     public double getHarga() {
         return harga;
     }
+
     public void setHarga(double harga) {
         this.harga = harga;
     }
